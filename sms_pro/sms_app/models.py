2716a7430b2e85b7b77d2b63ce65a406590a82c0
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-12-03 11:33:10 +0530

    sms

--- sms_pro/sms_app/models.py
+++ sms_pro/sms_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    rollno=models.IntegerField()
+    name=models.CharField(max_length=30)
+    age=models.IntegerField()
+    mark=models.IntegerField()
+    address=models.TextField()
--- sms_pro/sms_app/views.py
+++ sms_pro/sms_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import *
+from .seializers import *
 # Create your views here.
+
+class std_view(viewsets.ModelViewSet):
+    queryset=Student.objects.all()
+    serializer_class=Stud
